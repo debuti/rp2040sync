a72e6f9f5beb32f9cd7ab24122e4309fe066f21b
Borja García <debuti@gmail.com>
2021-06-09 00:48:23 +0200

    First version

--- src/sync/atomic.rs
+++ src/sync/atomic.rs
@@ -1,26 +1,69 @@
 use core::sync::atomic::Ordering;
+use core::cell::UnsafeCell;
 
-
-pub struct Atomic<T> where T: PartialEq + Copy {
-    inner: T,
+pub struct Atomic<T> {
+    inner: UnsafeCell<T>,
 }
 
 const SIO_BASE: usize = 0xd000_0000;
 const SPINLOCK_OFFSET: usize = 0x100;
 const SPINLOCKS_COUNT: usize = 32;
 
-type AtomicUsize = Atomic<usize>;
-type AtomicBool = Atomic<bool>;
+pub type AtomicUsize = Atomic<usize>;
+pub type AtomicBool = Atomic<bool>;
 
-impl<T> Atomic<T> where T: PartialEq + Copy{
-    pub fn new(initial: T) -> Self {
-      Self {
-        inner : initial,
-      }
+impl<T> Atomic<T> {
+    pub const fn new(initial: T) -> Self {
+        Self { inner : UnsafeCell::new(initial) }
+    }
+}
+
+impl<T> Atomic<T> where T: PartialEq 
+    + Copy {
+
+    pub fn load(
+        &self,
+         _: Ordering
+    ) -> T {
+        let result;
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            result = *self.inner.get();
+            // Spinlock release
+            core::ptr::write(sl, 0);
+        }
+        result
+    }
+
+    pub fn store(
+        &self,
+        value: T,
+         _: Ordering
+    ) {
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            *self.inner.get() = value;
+            // Spinlock release
+            core::ptr::write(sl, 0);
+        }
+    }
+    
+    pub fn get_mut(&mut self) -> &mut T {
+        self.inner.get_mut()
     }
 
     pub fn compare_exchange(
-        &mut self,
+        &self,
         current: T,
         new: T,
         _success: Ordering,
@@ -31,18 +74,109 @@ impl<T> Atomic<T> where T: PartialEq + Copy{
             let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
             let mut res = 0;
             while res == 0 {
-                res = std::ptr::read(sl);
+                res = core::ptr::read(sl);
             }
             // Spinlock locked
-            if self.inner == current {
-                self.inner = new;
+            if *self.inner.get() == current {
+                *self.inner.get() = new;
                 result = Ok(new);
             }
             else {
                 result = Err(current);
             }
             // Spinlock release
-            std::ptr::write(sl, 0);
+            core::ptr::write(sl, 0);
+        }
+        result
+    }
+    
+    pub fn compare_exchange_weak(
+        &self,
+        current: T,
+        new: T,
+        _success: Ordering,
+        _failure: Ordering,
+    ) -> Result<T, T> {
+      self.compare_exchange(current, new, _success, _failure)
+    }
+
+}
+
+
+impl<T> Atomic<T> where T: PartialEq 
+                          + Copy
+                          + core::ops::BitAndAssign
+                          + core::ops::BitOrAssign {
+    
+    pub fn fetch_and(&self, val: T, _: Ordering) -> T {
+        let result;
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            result = *self.inner.get();
+            *self.inner.get() &= val;
+            // Spinlock release
+            core::ptr::write(sl, 0);
+        }
+        result
+    }
+
+    pub fn fetch_or(&self, val: T, _: Ordering) -> T {
+        let result;
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            result = *self.inner.get();
+            *self.inner.get() |= val;
+            // Spinlock release
+            core::ptr::write(sl, 0);
+        }
+        result
+    }
+}
+
+impl<T> Atomic<T> where T: PartialEq 
+                        + Copy
+                        + core::ops::AddAssign
+                        + core::ops::SubAssign {
+    pub fn fetch_add(&self, val: T, _: Ordering) -> T {
+        let result;
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            result = *self.inner.get();
+            *self.inner.get() += val;
+            // Spinlock release
+            core::ptr::write(sl, 0);
+        }
+        result
+    }
+
+    pub fn fetch_sub(&self, val: T, _: Ordering) -> T {
+        let result;
+        unsafe {
+            let sl = (SIO_BASE+SPINLOCK_OFFSET) as *mut u32;
+            let mut res = 0;
+            while res == 0 {
+                res = core::ptr::read(sl);
+            }
+            // Spinlock locked
+            result = *self.inner.get();
+            *self.inner.get() -= val;
+            // Spinlock release
+            core::ptr::write(sl, 0);
         }
         result
     }
